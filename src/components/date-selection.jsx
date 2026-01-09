2111b177f8045a4a3e7869f6524c5a7aaf0d0d51
Álex Bryan <bryannascimentopl800@gmail.com>
2026-01-08 22:10:12 -0300

    feat: ensure selected date is persisted on page reload

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,15 +1,36 @@
 import { addMonths } from 'date-fns';
-import { useState } from 'react';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
 
 import { DatePickerWithRange } from './ui/date-picker-with-range';
 
+const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd');
+
 const DateSelection = () => {
-  const [date] = useState({
-    from: new Date(),
-    to: addMonths(new Date(), 1),
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const [date, setDate] = useState({
+    from: searchParams.get('from')
+      ? new Date(searchParams.get('from') + 'T00:00:00')
+      : new Date(),
+    to: searchParams.get('to')
+      ? new Date(searchParams.get('to') + 'T00:00:00')
+      : addMonths(new Date(), 1),
   });
 
-  return <DatePickerWithRange value={date} />;
+  useEffect(() => {
+    if (!date?.from || !date?.to) return;
+    const queryParams = new URLSearchParams();
+    queryParams.set('from', formatDateToQueryParam(date.from));
+    queryParams.set('to', formatDateToQueryParam(date.to));
+    if (date.from && date.to) {
+      navigate(`/?${queryParams.toString()}`);
+    }
+  }, [navigate, date]);
+
+  return <DatePickerWithRange value={date} onChange={setDate} />;
 };
 
 export default DateSelection;
